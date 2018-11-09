a0c3916186694e1d774997d387f779039bcddba1
zan <zan_zibert@hotmail.com>
2018-11-09 13:10:34 +0100

    #10 - Data Binding

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  homeTitle = 'this is the home title'
+
   constructor() { }
 
   ngOnInit() {
